02ad1d0f0f28f78785bac1c19c43517b3455f122
petathome <82181432+zacno2711@users.noreply.github.com>
2023-06-08 00:10:48 -0500

    Validacion de formulario

--- src/Agenda/Agenda.jsx
+++ src/Agenda/Agenda.jsx
@@ -1,32 +1,128 @@
 import React from 'react';
+import { useState, useEffect } from "react";
+// import Swal from 'sweetalert2';
 import { Rutas } from '../Rutas/Rutas';
+import Swal from 'sweetalert2';
 export function Agenda() {
+    const [inputDia, setInputDia] = useState(null);
+    const [inputHora, setInputHora] = useState(null);
+    const [inputNombre, setInput] = useState(null);
+    const [inputApellido, setInputApellido] = useState(null);
+    const [inputCorreo, setInputCorreo] = useState(null);
+    const [errores, setErrores] = useState({});
+
+    const [envioDeFormulario, setEnvioDeFormulario] = useState(false);
+    const [reservas, setReservas] = useState(null);
+    const [carga, setCarga] = useState(true);
+
+
+    useEffect(function(){
+        if (Object.keys(errores).length==0){
+            Swal.fire(
+                'Servicio adquirido',
+                'click me',
+                'success'
+            )
+        }
+
+        if (envioDeFormulario) {
+            Swal.fire({
+                icon: "error",
+                title: "xd",
+                text: "campos vacios"
+            })
+        }
+    })
+
+    function validarFormulario(evento) {
+        evento.preventDefault()
+        let listaErrores = []
+        if (!inputNombre) {
+            listaErrores.nombre = "El nombre esta vacio."
+        }
+        if (!inputApellido) {
+            listaErrores.apellido = "El apellido esta vacio."
+        }
+        if (!inputCorreo) {
+            listaErrores.correo = "El correo esta vacio."
+        }
+        if (!inputDia) {
+            listaErrores.dia = "El dia esta vacio."
+        }
+        if (!inputHora) {
+            listaErrores.hora = "El hora esta vacia."
+        }
+        setErrores(listaErrores);
+        setEnvioDeFormulario(true);
+    }
+
+
     return (
         <div className="d-flex justify-content-center align-items-center">
             <div>
                 <h1>Formulario de Agendamiento</h1>
-                <form action="#" method="post">
+                <form onSubmit={validarFormulario}>
+                    
                     <div className="form-group">
-                        <label htmlFor="dia">Día:</label>
-                        <input type="text" className="form-control form-control-sm" id="dia" name="dia" required />
+                        <label htmlFor="date" className="col-1 col-form-label">Dia:</label>
+                        <div className="col-5">
+                            <div className="input-group date" id="datepicker">
+                                <input 
+                                    type="date" 
+                                    className="form-control" 
+                                    id="date"
+                                    onChange={function(evento){setInputDia(evento.target.value)}}/>
+                            </div>
+                        </div>
+
                     </div>
 
                     <div className="form-group">
-                        <label htmlFor="hora">Hora:</label>
-                        <input type="text" className="form-control form-control-sm" id="hora" name="hora" required />
+                        <label htmlFor="date" className="col-1 col-form-label">Hora:</label>
+                        <select 
+                            defaultValue={"DEFAULT"}
+                            className ="form-select" 
+                            aria-label="Default select example"
+                            onChange={function(evento){setInputHora(evento.target.value)}}>
+
+                            <option selected>Seleccione la hora del servicio</option>
+                            <option value="1">10 am</option>
+                            <option value="2">1 pm</option>
+                            <option value="3">5 pm</option>
+                        </select>
                     </div>
 
                     <div className="form-group">
-                        <label htmlFor="nombres">Nombres:</label>
-                        <input type="text" className="form-control form-control-sm" id="nombres" name="nombres" required />
+                        <label htmlFor="nombre">Nombre:</label>
+                        <input 
+                            type="text" 
+                            className="form-control form-control-sm" 
+                            id="nombre" 
+                            name="nombre"  
+                            onChange={function(evento){setInputNombre(evento.target.value)}}/>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="apellido">Apellido:</label>
+                        <input 
+                            type="text" 
+                            className="form-control form-control-sm" 
+                            id="apellido" 
+                            name="apellido"
+                            onChange={function(evento){setInputApellido(evento.target.value)}}  />
                     </div>
 
                     <div className="form-group">
                         <label htmlFor="correo">Correo:</label>
-                        <input type="email" className="form-control form-control-sm" id="correo" name="correo" required />
+                        <input 
+                            type="email" 
+                            className="form-control form-control-sm" 
+                            id="correo" 
+                            name="correo"
+                            onChange={function(evento){setInputCorreo(evento.target.value)}}  />
                     </div>
 
-                    <input type="submit" value="Enviar" className="btn btn-primary" style={{ marginTop: '10px' }} />
+                    <button type='submit' className='btn btn-primary w-100'>AGENDAR</button>
                 </form>
             </div>
         </div>
